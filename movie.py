89178a08a9358843e2486cd75dc121aa73d4c0da
sonalparab <sparab@stuy.edu>
2017-11-25 18:49:32 -0500

    Worked on movie searches

--- movie.py
+++ movie.py
@@ -1,20 +1,58 @@
 import urllib2
 import json
+import key
 
+app_key = key.nytimes_key
+
+'''Takes in the name of the movie and returns the entire 
+dict of results which has not been filtered through'''
 def search(name):
-    link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=48a9c92b946e4c09b902e97f12663af1&query=https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=48a9c92b946e4c09b902e97f12663af1&query='" + name + "'"
+    link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + app_key + "&query=" + name
+    u = urllib2.urlopen(link)
+    d = json.loads(u.read())
+    return d
+
+'''Takes in the name of the movie and returns a dict
+of only the given name if it exists'''
+def advancedSearch(name):
+    link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + app_key + "&query='" + name + "'"
     u = urllib2.urlopen(link)
     d = json.loads(u.read())
     if(d['num_results'] == 0):
-        print("Results")
-        link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=48a9c92b946e4c09b902e97f12663af1&query=https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=48a9c92b946e4c09b902e97f12663af1&query=" + name
-        u = urllib2.urlopen(link)
-        d = json.loads(u.read())
+        print("No corresponding book was found")
     else:
-        print("We found a movie with that exact title!")
-    print link
-    print d
+        print("Corresponding book found!")
     return d
 
-#search('wonders')
+'''Builds the dictionary of results which is cleaner than that
+returned by the NYTimes API. The name of the movie is the key 
+and the review information is the value as a subdict'''
+def getResultsDict(info):
+    ans = {}
+    for entry in info['results']:
+        #making the entries keys strings to 
+        # avoid key errors when accessing by the title
+        t = "" + entry['display_title']
+        title = t
+        d = {}
+        d['reviewLink'] = entry['link']['url']
+        d['reviewTitle'] = entry['headline']
+        d['reviewAuthor'] = entry['byline']
+        d['rating'] = entry['mpaa_rating']
+        #sometimes release date is None
+        d['released'] = entry['opening_date']
+        ans[entry['display_title']] = d
+    return ans
+        
+
+#example test cases
 dict = search('wonder')
+dict2 = advancedSearch('wonder')
+d = getResultsDict(dict)
+d2 = getResultsDict(dict2)
+for entry in d:
+    #str = "" + entry + ""
+    print entry
+    print d[entry]
+print
+print d2
