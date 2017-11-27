4c22a712a44b47cbc2bcbcaf1b3f0728d4a6f08e
sonalparab <sparab@stuy.edu>
2017-11-27 13:54:46 -0500

    Movie searches

--- movie.py
+++ movie.py
@@ -1,5 +1,4 @@
-import urllib2
-import json
+import json, requests
 import key
 
 app_key = key.nytimes_key
@@ -7,26 +6,31 @@ app_key = key.nytimes_key
 '''Takes in the name of the movie and returns the entire
 dict of results which has not been filtered through'''
 def search(name):
-    link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + app_key + "&query=" + name
-    u = urllib2.urlopen(link)
-    d = json.loads(u.read())
-    return d
+    url =  "https://api.nytimes.com/svc/movies/v2/reviews/search.json"
+    info_d = {'api-key': app_key, 'query':name}
+    r = requests.get(url, params=info_d)
+    d = r.json()
+    if(d['num_results'] == 0):
+        return None
+    else:
+        return getResultsDict(d)
 
 '''Takes in the name of the movie and returns a dict
 of only the given name if it exists'''
 def advancedSearch(name):
-    link = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + app_key + "&query='" + name + "'"
-    u = urllib2.urlopen(link)
-    d = json.loads(u.read())
-    #if(d['num_results'] == 0):
-        #print("No corresponding book was found")
-    #else:
-        #print("Corresponding book found!")
-    return d
+    url =  "https://api.nytimes.com/svc/movies/v2/reviews/search.json"
+    q = "'" + name + "'"
+    info_d = {'api-key': app_key, 'query':q}
+    r = requests.get(url, params=info_d)
+    d = r.json()
+    if(d['num_results'] == 0):
+        return None
+    else:
+        return getResultsDict(d)
 
-'''Builds the dictionary of results which is cleaner than that
-returned by the NYTimes API. The name of the movie is the key
-and the review information is the value as a subdict'''
+'''Helper Function: Builds the dictionary of results which is cleaner
+than the one returned by the NYTimes API. The name of the movie is the
+key and the review information is the value as a subdict'''
 def getResultsDict(info):
     ans = {}
     for entry in info['results']:
@@ -38,7 +42,7 @@ def getResultsDict(info):
         d['reviewLink'] = entry['link']['url']
         d['reviewTitle'] = entry['headline']
         d['reviewAuthor'] = entry['byline']
-        d['reviewSummary'] = entry['summary_short']
+        d['summary'] = entry['summary_short']
         d['rating'] = entry['mpaa_rating']
         #sometimes release date is None
         d['released'] = entry['opening_date']
@@ -47,13 +51,12 @@ def getResultsDict(info):
 
 
 #example test cases
+'''
 dict = search('wonder')
 dict2 = advancedSearch('wonder')
-d = getResultsDict(dict)
-d2 = getResultsDict(dict2)
-#for entry in d:
-    #str = "" + entry + ""
-    #print entry
-    #print d[entry]
-#print
-#print d2
+for entry in dict:
+    print entry
+    print dict[entry]
+print
+print dict2
+'''
