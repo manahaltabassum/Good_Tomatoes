87b6ddb35c478582bb89be3ce326c1005ee41b49
Angelica Zverovich <azverovich@stuy.edu>
2017-11-27 04:24:19 -0500

    merge conflicts/added search bars and working on app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, redirect
+import book, movie
 
 app = Flask(__name__)
 
 @app.route('/')
 def root ():
-    return 'Welcome'
+    return render_template ('welcome.html')
+
+@app.route('/movie')
+def movie():
+    return render_template('movie.html');
+
+@app.route('/book')
+def book():
+    return render_template('book.html');
+
+
+@app.route('/searchedbook')
+def searchedbook():
+    search = request.form['']
+    return render_template("bookreviews.html", search = search)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
